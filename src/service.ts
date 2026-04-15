7fda3f4e888b9205ace00d4a7c1d88c205a9b394
brady-arsenault <bvarsenault@umass.edu>
2026-04-15 12:56:42 -0400

    fix: make EventService functions async

--- src/service.ts
+++ src/service.ts
@@ -2,39 +2,39 @@ import { CancelEventError, CreateEventError, CreateEventInput, GetEventByIdError
 import { Result } from "./lib/result";
 
 class EventService implements IEventService, IRsvpService {
-    toggleRsvp(eventId: string, actingUser: IActingUser): Promise<Result<IRsvp, ToggleRsvpError>> {
-        throw new Error("Method not implemented.");
-    }
-    
-    getUserRsvps(actingUser: IActingUser): Promise<Result<IUserRsvpDashboard, GetUserRsvpsError>> {
+    async toggleRsvp(eventId: string, actingUser: IActingUser): Promise<Result<IRsvp, ToggleRsvpError>> {
         throw new Error("Method not implemented.");
     }
 
-    createEvent(input: CreateEventInput, actingUser: IActingUser): Promise<Result<IEvent, CreateEventError>> {
+    async getUserRsvps(actingUser: IActingUser): Promise<Result<IUserRsvpDashboard, GetUserRsvpsError>> {
         throw new Error("Method not implemented.");
     }
 
-    getEventById(eventId: string, actingUser: IActingUser): Promise<Result<IEvent, GetEventByIdError>> {
+    async createEvent(input: CreateEventInput, actingUser: IActingUser): Promise<Result<IEvent, CreateEventError>> {
         throw new Error("Method not implemented.");
     }
 
-    updateEvent(eventId: string, input: UpdateEventInput, actingUser: IActingUser): Promise<Result<IEvent, UpdateEventError>> {
+    async getEventById(eventId: string, actingUser: IActingUser): Promise<Result<IEvent, GetEventByIdError>> {
         throw new Error("Method not implemented.");
     }
 
-    publishEvent(eventId: string, actingUser: IActingUser): Promise<Result<IEvent, PublishEventError>> {
+    async updateEvent(eventId: string, input: UpdateEventInput, actingUser: IActingUser): Promise<Result<IEvent, UpdateEventError>> {
         throw new Error("Method not implemented.");
     }
 
-    cancelEvent(eventId: string, actingUser: IActingUser): Promise<Result<IEvent, CancelEventError>> {
+    async publishEvent(eventId: string, actingUser: IActingUser): Promise<Result<IEvent, PublishEventError>> {
         throw new Error("Method not implemented.");
     }
 
-    listEvents(filter: ListEventsFilter): Promise<Result<IEvent[], ListEventsError>> {
+    async cancelEvent(eventId: string, actingUser: IActingUser): Promise<Result<IEvent, CancelEventError>> {
         throw new Error("Method not implemented.");
     }
 
-    searchEvents(input: SearchEventsInput): Promise<Result<IEvent[], SearchEventsError>> {
+    async listEvents(filter: ListEventsFilter): Promise<Result<IEvent[], ListEventsError>> {
+        throw new Error("Method not implemented.");
+    }
+
+    async searchEvents(input: SearchEventsInput): Promise<Result<IEvent[], SearchEventsError>> {
         throw new Error("Method not implemented.");
     }
 
